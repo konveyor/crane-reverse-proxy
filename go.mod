0bbbc5f396bad15878c2af0748d143568e6c3cff
Jason Montleon <jmontleo@redhat.com>
2022-01-11 12:46:10 -0500

    big rewrite and simplification

--- go.mod
+++ go.mod
@@ -1,3 +1,32 @@
 module github.com/jmontleon/reverse-proxy-poc
 
 go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/konveyor/mig-controller v0.0.0-20220110144829-0b1d57511b4c
+	k8s.io/api v0.21.3
+	k8s.io/apimachinery v0.21.3
+	k8s.io/client-go v0.22.1
+	sigs.k8s.io/controller-runtime v0.9.2
+)
+
+replace bitbucket.org/ww/goautoneg v0.0.0-20120707110453-75cd24fc2f2c => github.com/markusthoemmes/goautoneg v0.0.0-20190713162725-c6008fefa5b1
+
+replace github.com/gogo/protobuf => github.com/gogo/protobuf v1.3.2
+
+replace k8s.io/client-go => k8s.io/client-go v0.20.7
+
+replace k8s.io/apimachinery => k8s.io/apimachinery v0.20.7
+
+replace k8s.io/api => k8s.io/api v0.20.7
+
+replace k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.20.7
+
+replace k8s.io/apiserver => k8s.io/apiserver v0.20.7
+
+replace k8s.io/cli-runtime => k8s.io/cli-runtime v0.20.7
+
+replace sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.7.1-0.20201215171748-096b2e07c091
+
+replace github.com/konveyor/mig-controller => /home/jason/Documents/src/github.com/konveyor/mig-controller
--- main.go
+++ main.go
@@ -1,21 +1,86 @@
 package main
 
 import (
-	"log"
+	"context"
+	"crypto/tls"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
-	"github.com/jmontleon/reverse-proxy-poc/pkg/rproxy"
+	"github.com/gin-gonic/gin"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
+	migapi "github.com/konveyor/mig-controller/pkg/apis/migration/v1alpha1"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	k8sconfig "sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 func main() {
-	proxy, err := rproxy.NewProxy(os.Getenv("CLUSTER_URL"))
+	scheme := runtime.NewScheme()
+
+	if err := migapi.AddToScheme(scheme); err != nil {
+		panic(err)
+	}
+	if err := v1.AddToScheme(scheme); err != nil {
+		panic(err)
+	}
+
+	config := k8sconfig.GetConfigOrDie()
+
+	client, err := k8sclient.New(config, k8sclient.Options{Scheme: scheme})
 	if err != nil {
 		panic(err)
 	}
-	http.HandleFunc("/", rproxy.ProxyRequestHandler(proxy))
-	log.Fatal(http.ListenAndServeTLS(":8080",
-		os.Getenv("TLSCrt"),
-		os.Getenv("TLSKey"),
-		nil))
+
+	r := gin.Default()
+
+	ref := types.NamespacedName{
+		Namespace: "openshift-migration",
+		Name:      "migration-controller",
+	}
+
+	controller := migapi.MigrationController{}
+
+	err = client.Get(context.TODO(), ref, &controller)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, cluster := range controller.Spec.Clusters {
+		var remote *url.URL
+		var err error
+
+		ref := types.NamespacedName{
+			Namespace: cluster.Namespace,
+			Name:      cluster.Name,
+		}
+
+		secret := v1.Secret{}
+
+		err = client.Get(context.TODO(), ref, &secret)
+		if err != nil {
+			panic(err)
+		}
+
+		remote, err = url.Parse(string(secret.Data["url"]))
+		if err != nil {
+			panic(err)
+		}
+
+		r.Any("/"+cluster.Namespace+"/"+cluster.Name+"/*proxyPath", func(c *gin.Context) {
+			c.Request.URL.Path, _ = c.Params.Get("proxyPath")
+
+			proxy := httputil.NewSingleHostReverseProxy(remote)
+			proxy.Transport = &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+			proxy.ServeHTTP(c.Writer, c.Request)
+		})
+
+	}
+
+	r.RunTLS(":8080", os.Getenv("TLSCrt"), os.Getenv("TLSKey"))
 }
