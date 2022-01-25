3c8b96ce2677917dac91fdbd02d2556b0b5a56a5
Jason Montleon <jmontleo@redhat.com>
2022-01-24 23:49:43 -0500

    Rewrite logic

--- main.go
+++ main.go
@@ -28,11 +28,7 @@ type Cluster struct {
 }
 
 func main() {
-
-	namespace := os.Getenv("NAMESPACE")
-	if namespace == "" {
-		log.Fatalf("Please set the 'NAMESPACE' environment variable.")
-	}
+	clusterMap := make(map[string]*url.URL)
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -44,55 +40,113 @@ func main() {
 		log.Fatalf("Unable to create kubernetes client.")
 	}
 
+	clusterMapInit(client, &clusterMap)
+
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
+
+	r.Any("/:namespace/:name/*proxyPath", func(c *gin.Context) {
+		var proxy *httputil.ReverseProxy
+
+		namespace, _ := c.Params.Get("namespace")
+		name, _ := c.Params.Get("name")
+
+		if url, ok := clusterMap[namespace+name]; ok {
+			proxy = httputil.NewSingleHostReverseProxy(url)
+		} else {
+			url := clusterMapAdd(client, &clusterMap, namespace, name)
+			if url != nil {
+				proxy = httputil.NewSingleHostReverseProxy(url)
+			}
+		}
+
+		if proxy == nil {
+			c.AbortWithStatus(http.StatusBadGateway)
+		} else {
+			proxy.Transport = &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+
+			c.Request.URL.Path, _ = c.Params.Get("proxyPath")
+			c.Request.Host = clusterMap[namespace+name].Host
+
+			proxy.ServeHTTP(c.Writer, c.Request)
+
+			if c.Writer.Status() == http.StatusBadGateway {
+				clusterMapRemove(&clusterMap, namespace, name)
+			}
+		}
+	})
+
+	r.Run(":8080")
+}
+
+func clusterMapInit(client client.Client, clusterMap *map[string]*url.URL) {
+
+	namespace := os.Getenv("NAMESPACE")
+	configmap := v1.ConfigMap{}
+	clusters := []Cluster{}
+
+	if namespace == "" {
+		log.Fatalf("Please set the 'NAMESPACE' environment variable.")
+	}
 
 	ref := types.NamespacedName{
 		Namespace: namespace,
 		Name:      proxySecretName,
 	}
 
-	configmap := v1.ConfigMap{}
-	err = client.Get(context.TODO(), ref, &configmap)
+	err := client.Get(context.TODO(), ref, &configmap)
 	if err != nil {
 		log.Fatalf("Unable to load ConfigMap: %s in Namespace: %s", proxySecretName, namespace)
 	}
 
-	clusters := []Cluster{}
-
 	json.Unmarshal([]byte(configmap.Data["clusters"]), &clusters)
 
 	for _, cluster := range clusters {
-		var remote *url.URL
-		var err error
-
 		ref := types.NamespacedName{
 			Namespace: cluster.Namespace,
 			Name:      cluster.Name,
 		}
 
 		secret := v1.Secret{}
-		err = client.Get(context.TODO(), ref, &secret)
+		err := client.Get(context.TODO(), ref, &secret)
+
 		if err != nil {
-			log.Printf("Unable to retrieve secret %s in Namespace: %s. No proxy created for this clsuter.", cluster.Name, cluster.Namespace)
+			log.Printf("Unable to retrieve secret %s in Namespace: %s. No proxy created for this cluster.", cluster.Name, cluster.Namespace)
 		}
 
-		remote, err = url.Parse(string(secret.Data["url"]))
+		remote, err := url.Parse(string(secret.Data["url"]))
 		if err != nil {
-			log.Printf("No URL found in secret %s in Namespace: %s. No proxy created for this clsuter.", cluster.Name, cluster.Namespace)
+			log.Printf("No URL found in secret %s in Namespace: %s. No proxy created for this cluster.", cluster.Name, cluster.Namespace)
 		}
 
-		r.Any("/"+cluster.Namespace+"/"+cluster.Name+"/*proxyPath", func(c *gin.Context) {
-			c.Request.URL.Path, _ = c.Params.Get("proxyPath")
+		(*clusterMap)[cluster.Namespace+cluster.Name] = remote
+	}
+}
 
-			proxy := httputil.NewSingleHostReverseProxy(remote)
-			proxy.Transport = &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			}
-			proxy.ServeHTTP(c.Writer, c.Request)
-		})
-
+func clusterMapAdd(client client.Client, clusterMap *map[string]*url.URL, namespace string, name string) *url.URL {
+	ref := types.NamespacedName{
+		Namespace: namespace,
+		Name:      name,
 	}
 
-	r.Run(":8080")
+	secret := v1.Secret{}
+	err := client.Get(context.TODO(), ref, &secret)
+	if err != nil {
+		return nil
+	}
+
+	remote, err := url.Parse(string(secret.Data["url"]))
+	if err != nil {
+		return nil
+	}
+
+	(*clusterMap)[namespace+name] = remote
+
+	return (*clusterMap)[namespace+name]
+}
+
+func clusterMapRemove(clusterMap *map[string]*url.URL, namespace string, name string) {
+	delete((*clusterMap), namespace+name)
 }
